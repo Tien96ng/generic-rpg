bd179198b21945d8932026098f566a4226b7ef07
tien96ng <tien96ng@ghmail.com>
2021-02-04 11:41:35 -0800

    Test pass for Character class.

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,8 +1,17 @@
 import Game from "../src/js/game.js";
+import Character from "../src/js/character.js";
 
 describe("Game", () => {
   test("Should return a game with player1 created.", () => {
     const newGame =  new Game("player1");
     expect(newGame.player1).toEqual("player1");
+  })
+});
+
+describe("Character", () => {
+  test("Should return new character with their name and their choosen class.", () => {
+    const newCharacter =  new Character("Cloud", "Swordsman");
+    expect(newCharacter.name).toEqual("Cloud");
+    expect(newCharacter.characterClass).toEqual("Swordsman");
   })
 });
